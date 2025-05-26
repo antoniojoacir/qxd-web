c4a8f7feffbcf6aa67c55282a49e5fb893c03010
antoniojoacir <antoniojoacir@gmail.com>
2025-05-26 09:25:54 -0300

    promise with awai and async

--- javascript/promise/index.js
+++ javascript/promise/index.js
@@ -1,4 +1,4 @@
-const getPromiseAPI = (url) => {
+/*const getPromiseAPI = (url) => {
   let promise = new Promise((RESOLVE, REJECT) => {
     setTimeout(() => {
       const num = 3;
@@ -19,4 +19,32 @@ const clientAPI = () => {
     });
 };
 
+const emulando_outra_comp = () => {
+  console.log("EMULANDO OUTRA COMPUTACAO");
+};
+
 clientAPI();
+emulando_outra_comp();
+*/
+
+const getPromiseAPI = (url) => {
+  let promise = new Promise((RESOLVE, REJECT) => {
+    setTimeout(() => {
+      const num = 3;
+      if (num % 2 === 0) RESOLVE("OK: NUMERO PAR");
+      else REJECT("NOK: NUMERO IMPAR");
+    }, 3000);
+  });
+  return promise;
+};
+
+const clientAPI = async () => {
+    try {
+        const res = await getPromiseAPI("https://nothing.com/dados")
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+clientAPI()
